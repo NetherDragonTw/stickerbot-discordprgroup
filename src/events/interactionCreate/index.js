51540e9d363d43069b27f977c73309ed733c973e
NetherDragon_Tw <Twor1060595152@hotmail.com>
2023-03-25 22:16:37 +0800

    Interactioncreate程式碼修正

--- src/events/interactionCreate/index.js
+++ src/events/interactionCreate/index.js
@@ -2,13 +2,13 @@ import { Events } from "discord.js"
 import { useAppStore } from '@/store/app'
 
 export const event = {
-    name: Events.interactionCreate
+    name: Events.InteractionCreate
 }
 
 export const action = async (interaction) => {
     if(!interaction.isChatInputCommand()) return
     const appStore = useAppStore()
-    const action = appStore.commandActionMap.get(interaction.commandName)
+    const action = appStore.commandsActionMap.get(interaction.commandName)
 
     await action()
 }
